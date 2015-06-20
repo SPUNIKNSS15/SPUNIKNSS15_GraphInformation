41c027c3aef594595714d634c340623fcf0c5d2e
Moritz Renftle <kruscht@renftles.de>
2015-06-20 23:40:42 +0200

    add the new getAutomorphisms() test from unit_test_base

--- tests/isgci/smallgraph/ConfigurationTest.java
+++ tests/isgci/smallgraph/ConfigurationTest.java
@@ -1,9 +1,9 @@
 package tests.isgci.smallgraph;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import teo.isgci.smallgraph.Configuration;
 import teo.isgci.smallgraph.Graph;
 
@@ -11,8 +11,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -41,23 +40,57 @@ public class ConfigurationTest {
     private Configuration conf;
 
     /**
-     * Class describing the differences between the representatives
-     * of a configuration sample and the current implementation.
+     * Class describing the differences between two sets of objects,
+     * one coming from the sample and one from the current implementation.
      */
-    private class RepDiff {
+    private class Diff<E> {
         public final int sampleNo;
         public final int both;
-        public final ArrayList<Graph> sampleOnly;
-        public final ArrayList<Graph> internalOnly;
+        public final List<E> sampleOnly;
+        public final List<E> internalOnly;
 
-        public RepDiff(int sampleNo,
-                       int both,
-                       ArrayList<Graph> sampleOnly,
-                       ArrayList<Graph> internalOnly) {
+        /**
+         * Set up the diff.
+         *
+         * @param sampleNo the sample the diff points against
+         * @param sampleList the list of objects specified by the sample
+         * @param internalList the list of objects calculated by the implementation
+         */
+        public Diff(int sampleNo,
+                    List<E> sampleList,
+                    List<E> internalList) {
+            this(sampleNo, sampleList, internalList, (o1, o2) -> o1.equals(o2) ? 0 : -1);
+        }
+
+        /**
+         * Set up the diff using a special comparison function.
+         *
+         * @param sampleNo the sample the diff points against
+         * @param sampleList the list of objects specified by the sample
+         * @param internalList the list of objects calculated by the implementation
+         * @param c the comparison function for objects in the lists (only
+         *          equality matters)
+         */
+        public Diff(int sampleNo,
+                    List<E> sampleList,
+                    List<E> internalList,
+                    Comparator<E> c) {
+            int both = 0;
+            for (Iterator<E> sampleIt = sampleList.iterator(); sampleIt.hasNext();) {
+                for (Iterator<E> internalIt = internalList.iterator(); internalIt.hasNext();) {
+                    E sampleObj = sampleIt.next();
+                    E internalObj = internalIt.next();
+                    if (c.compare(sampleObj, internalObj) == 0) {
+                        sampleIt.remove();
+                        internalIt.remove();
+                        both++;
+                    }
+                }
+            }
             this.sampleNo = sampleNo;
             this.both = both;
-            this.sampleOnly = sampleOnly;
-            this.internalOnly = internalOnly;
+            this.sampleOnly = sampleList;
+            this.internalOnly = internalList;
         }
 
         /**
@@ -81,6 +114,13 @@ public class ConfigurationTest {
         edgesPattern = Pattern.compile("\\[.*\\]");
     }
 
+    @Rule
+    public TestRule watcher = new TestWatcher() {
+        protected void starting(Description description) {
+            System.out.println("--> Running test: " + description.getMethodName());
+        }
+    };
+
     @Before
     public void setUp() throws Exception {
         conf = new Configuration();
@@ -88,6 +128,31 @@ public class ConfigurationTest {
 
     @After
     public void tearDown() throws Exception {}
+
+    /**
+     * Prints out a nice tabular evaluation of a set of Diff-objects,
+     *
+     * @param diffList Diff objects to evaluate
+     */
+    private void evaluateDiffList(List<Diff> diffList) {
+        if (diffList.size() > 0) {
+            System.out.println("Some samples could not be matched.\n");
+            System.out.format("%20s%20s%15s%15s\n", "Sample no", "In Sample only", "Internal only", "Both");
+
+            for (Diff d : diffList) {
+                System.out.format("%20s%20d%15d%15d\n", d.sampleNo, d.sampleOnly.size(), d.internalOnly.size(), d.both);
+            }
+
+            System.out.println();
+            System.out.format("%10s%10d%20d%15d%15d\n", "Total:",
+                    diffList.size(),
+                    diffList.stream().map(d -> d.sampleOnly.size()).reduce(0, Integer::sum),
+                    diffList.stream().map(d -> d.internalOnly.size()).reduce(0, Integer::sum),
+                    diffList.stream().map(d -> d.both).reduce(0, Integer::sum));
+        } else {
+            System.out.println("All samples could be matched.");
+        }
+    }
 
     /**
      * Matches against a pattern and returns the first match.
@@ -216,64 +281,51 @@ public class ConfigurationTest {
     }
 
     /**
-     * Reads in a sample from "tests/data/configsamples" and
-     * sets up the internal Configuration object according to the sample spec.
-     * Then Configuration.getGraphs() is matched against the graphs in the
-     * sample.
+     * Reads in a sample from "tests/data/configsamples" and sets
+     * up the internal Configuration object according to the sample spec.
+     * Returns a list of all the specs in the same order as found
+     * in the sample.
      *
      * @param sampleNo the number of the sample to parse
-     * @return a RepDiff object containing the representatives which could not be
-     * matched
+     * @return an array containing the different specs of the sample
      */
-    private RepDiff testGetGraphsConfigurationSample(int sampleNo) {
+    private String[] readSample(int sampleNo) throws IOException {
         String file = String.format(samplePath + "sample-configuration-%02d", sampleNo);
 
         // Parse the sample configuration
-        try {
-            String confSample = new String(Files.readAllBytes(Paths.get(file)), StandardCharsets.UTF_8);
+        String confSample = new String(Files.readAllBytes(Paths.get(file)), StandardCharsets.UTF_8);
+        String[] specs = confSample.split("(?m)^-- .*$");
+        setupConfigurationFromString(specs[0]);
 
-            String[] parts = confSample.split("contains");
-            String confSpec = parts[0];
-            String contained = parts[1].split("induces")[0];
-
-            setupConfigurationFromString(confSpec);
-
-            ArrayList<Graph> confGraphs = new ArrayList<>();
-            ArrayList<String> graphSpecs = new ArrayList<>(Arrays.asList(contained.trim().split("\\n")));
-
-            // create all graphs which are known to be in the configuration
-            for (String graphSpec : graphSpecs) {
-                confGraphs.add(createGraphFromString(graphSpec));
-            }
-
-            ArrayList<Graph> calculatedGraphs= new ArrayList<>(conf.getGraphs());
-            int both = 0;
-
-            // search isomorphic Graph in calculatedGraphs for
-            // each graph in confGraphs. Then drop both.
-            for (Graph sampleGraph : new ArrayList<>(confGraphs)) {
-                for (Graph calculatedGraph : new ArrayList<>(calculatedGraphs)) {
-                    if (sampleGraph.isIsomorphic(calculatedGraph)) {
-                        confGraphs.remove(sampleGraph);
-                        calculatedGraphs.remove(calculatedGraph);
-                        both++;
-                    }
-                }
-            }
-
-            return new RepDiff(sampleNo, both, confGraphs, calculatedGraphs);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return specs;
     }
 
     /**
-     * Uses the configuration sample files from data/configsamples
-     * to initialize Configuration-objects and then match the result
-     * of Configuration.getGraphs() against the correct set of graphs
-     * using isomorphism tests.
+     * Matches Configuration.getGraphs() against the representatives of one sample.
+     *
+     * @param sampleNo the number of the sample to parse
+     * @return a Diff object containing the representatives which could not be
+     * matched
+     */
+    private Diff<Graph> testGetGraphsConfigurationSample(int sampleNo) throws IOException {
+        String contained = readSample(sampleNo)[1];
+
+        List<Graph> confGraphs = new ArrayList<>();
+        List<String> graphSpecs = new ArrayList<>(Arrays.asList(contained.trim().split("\n")));
+
+        // create all graphs which are known to be in the configuration
+        for (String graphSpec : graphSpecs) {
+            confGraphs.add(createGraphFromString(graphSpec));
+        }
+
+        List<Graph> calculatedGraphs= new ArrayList<>(conf.getGraphs());
+
+        return new Diff<>(sampleNo, confGraphs, calculatedGraphs, (g1, g2) -> g1.isIsomorphic(g2) ? 0 : -1);
+    }
+
+    /**
+     * For each sample, matches the representatives of Configuration.getGraphs()
+     * against the representatives of this sample.
      *
      * Prints detailed info on unmatched graphs.
      *
@@ -283,34 +335,61 @@ public class ConfigurationTest {
      */
     @Test
     public void testGetGraphs() throws Exception {
-        ArrayList<RepDiff> diffList = new ArrayList<>();
+        List<Diff> diffList = new ArrayList<>();
 
         for (int sampleNo = 0; sampleNo < numSamples; sampleNo++) {
-            RepDiff d = testGetGraphsConfigurationSample(sampleNo);
+            Diff d = testGetGraphsConfigurationSample(sampleNo);
             if (!d.isMatch()) {
                 diffList.add(d);
             }
         }
 
-        System.out.println("--- Testing getGraphs() ---");
+        evaluateDiffList(diffList);
 
-        if (diffList.size() > 0) {
-            System.out.println("Some samples could not be matched.\n");
-            System.out.format("%20s%20s%15s%15s\n", "Sample no", "In Sample only", "Internal only", "Both");
+        Assert.assertTrue(diffList.isEmpty());
+    }
 
-            for (RepDiff d : diffList) {
-                System.out.format("%20s%20d%15d%15d\n", d.sampleNo, d.sampleOnly.size(), d.internalOnly.size(), d.both);
-            }
+    /**
+     * Matches Configuration.getAutomorphisms() against the automorphisms
+     * of one sample.
+     *
+     * @param sampleNo the number of the sample to parse
+     * @return a Diff object containing the permutations which could not be
+     * matched
+     */
+    private Diff<String> testGetAutomorphismsConfigurationSample(int sampleNo) throws IOException {
+        List<String> sampleAutomorphisms = new ArrayList<>(Arrays.asList(readSample(sampleNo)[3].trim().split("\n")));
 
-            System.out.println();
-            System.out.format("%10s%10d%20d%15d%15d\n", "Total:",
-                            diffList.size(),
-                            diffList.stream().map(d -> d.sampleOnly.size()).reduce(0, Integer::sum),
-                            diffList.stream().map(d -> d.internalOnly.size()).reduce(0, Integer::sum),
-                            diffList.stream().map(d -> d.both).reduce(0, Integer::sum));
-        } else {
-            System.out.println("All samples could be matched.");
+        List<String> calculatedAutomorphisms = new ArrayList<>();
+        for (Object pObj : conf.getAutomorphisms()) {
+            calculatedAutomorphisms.add(Arrays.toString((int[]) pObj));
         }
+
+        return new Diff<>(sampleNo, sampleAutomorphisms, calculatedAutomorphisms);
+    }
+
+    /**
+     * For each sample, matches the result of Configuration.getAutomorphisms()
+     * against the automorphisms of this sample.
+     *
+     * Prints detailed info on unmatched permutations.
+     *
+     * See data/configsamples/README.md for info on the sample files.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetAutomorphisms() throws Exception {
+        List<Diff> diffList = new ArrayList<>();
+
+        for (int sampleNo = 0; sampleNo < numSamples; sampleNo++) {
+            Diff d = testGetAutomorphismsConfigurationSample(sampleNo);
+            if (!d.isMatch()) {
+                diffList.add(d);
+            }
+        }
+
+        evaluateDiffList(diffList);
 
         Assert.assertTrue(diffList.isEmpty());
     }
